1475132f2def1142da1626eab9615121b1719035
Christian M. Abuda <chankishiannn@gmail.com>
2025-08-08 01:58:26 +0800

    Update To-Do List.py

--- To-Do List.py
+++ To-Do List.py
@@ -1,1 +1,20 @@
+f = open("To-Do List Files.txt")
+
+
+print("========== To-Do List ==========")
+print(f.read())
+
+userChoose = input()
+
+while(userChoose != 5):
+    print("[1]  Add Task")
+    print("[2] Edit Task")
+    print("[3] Delete Task")
+    print("[4] View Tasks")
+    print("[5] Exitk")
+
+    match userChoose:
+        case 1:
+            
+            
 
