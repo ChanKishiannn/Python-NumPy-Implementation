4fc9b255eb7f2af3443f00ec909fff3e66456e97
Christian M. Abuda <chankishiannn@gmail.com>
2025-08-08 08:28:27 +0800

    Update To-Do List.py

--- To-Do List.py
+++ To-Do List.py
@@ -4,17 +4,22 @@ f = open("To-Do List Files.txt")
 print("========== To-Do List ==========")
 print(f.read())
 
-userChoose = input()
+userChoose = 0
 
 while(userChoose != 5):
     print("[1]  Add Task")
     print("[2] Edit Task")
     print("[3] Delete Task")
-    print("[4] View Tasks")
-    print("[5] Exitk")
+    print("[[4] View Tasks")
+    print("[5] Exit")
+    
+    userChoose = int(input("Choose an option: "))
+    print(userChoose)
 
-    match userChoose:
-        case 1:
+    if userChoose > 0 and userChoose < 5:
+        match userChoose:
+            case 1:   
+                task = input("Enter Task you want to add: ")
+                with open("To-Do List Files.txt", "a") as f:
+                    f.write(task)
             
-            
-
