271bce97a40efbba773576e28981fae44b4f8e1a
Christian M. Abuda <chankishiannn@gmail.com>
2025-08-22 14:15:58 +0800

    Update NumPy Multi-Dimensional-Indexing-practice.py

--- NumPy Multi-Dimensional-Indexing-practice.py
+++ NumPy Multi-Dimensional-Indexing-practice.py
@@ -6,5 +6,6 @@ arr = np.array([[["A","B","C"],["D","E","F"],["G","H","I"]],
 
 word = arr[0, 0, 2] + arr[0,2,1] + arr[0,0,0] + arr[1,1,1]
 
-#Printing the variable "word" that use multi-dimensional indexing to access the letters inside the array
+# Using multi-dimensional indexing to extract specific letters 
+# and combine them into a word ("CHAN")
 print(word) 
