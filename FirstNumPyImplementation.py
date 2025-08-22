e5d00c540e960bfbedb2d23edffe417be6c69916
Christian M. Abuda <chankishiannn@gmail.com>
2025-08-22 11:06:17 +0800

    Update and rename To-Do List Files.txt to FirstNumPyImplementation.py

---
+++ FirstNumPyImplementation.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+my_list = [1,2,3,4]
+numpyList = np.array([1,2,3,4])
+
+my_list = my_list * 2
+numpyList = numpyList * 2
+
+print(f"This is the without NumPy list: {my_list}")
+print(f"This is the NumPy list: {numpyList}")
