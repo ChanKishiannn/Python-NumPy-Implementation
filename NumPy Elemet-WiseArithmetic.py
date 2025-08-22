0803be22ad3fe17e0d0d268d3199152ec3cc4b56
Christian M. Abuda <chankishiannn@gmail.com>
2025-08-22 17:48:30 +0800

    Create NumPy Elemet-WiseArithmetic.py

---
+++ NumPy Elemet-WiseArithmetic.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+arr1 = np.array([1,2,3,4])
+arr2 = np.array([5,6,7,8])
+
+
+print(f"Addition: {arr1 + arr2}")
+print(f"Subraction: {arr1 - arr2}")
+print(f"Multiplication: {arr1 * arr2}")
+print(f"Division: {arr1 / arr2}")
+print(f"Squared: {arr1 ** arr2}")
