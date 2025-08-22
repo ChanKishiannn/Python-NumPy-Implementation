461d949fd83f328169b3f11dc50fc5c06bac1072
Christian M. Abuda <chankishiannn@gmail.com>
2025-08-22 16:34:45 +0800

    Create Numpy ScalarArithmetic.py

---
+++ Numpy ScalarArithmetic.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+arr = np.array([1,2,3,4])
+
+print(f"Addition: {arr + 2}")
+print(f"Subraction: {arr - 2}")
+print(f"Multiplication: {arr * 2}")
+print(f"Divition: {arr / 2}")
+print(f"Squared: {arr ** 2}")
